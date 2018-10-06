619726a2e33b6a42d4ba97550b51137bbfc9100b
Thomas <tai.okubo93@gmail.com>
2018-10-06 12:37:12 -0300

    Initial commit

---
+++ src/main/java/com/repnation/ufc/UfcApplication.java
@@ -0,0 +1,12 @@
+package com.repnation.ufc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfcApplication.class, args);
+	}
+}
