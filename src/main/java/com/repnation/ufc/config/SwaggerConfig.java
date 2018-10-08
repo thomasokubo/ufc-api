97907aa313de23f1bf8bc51d998b1390e09dddc3
Thomas <tai.okubo93@gmail.com>
2018-10-07 22:20:01 -0300

    Add swagger configuration class

--- src/main/java/com/repnation/ufc/config/SwaggerConfig.java
+++ src/main/java/com/repnation/ufc/config/SwaggerConfig.java
@@ -1,4 +1,50 @@
 package com.repnation.ufc.config;
 
-public class SwaggerConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport{
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.repnation.ufc"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("UFC API")
+                .description("REST API to provide information of UFC fights and fighters.")
+                .version("1.0.0")
+                .contact(new Contact("Thomas Okubo", "https://thomasokubo.github.io", "tai.okubo93@gmail.com"))
+                .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+
+
 }
