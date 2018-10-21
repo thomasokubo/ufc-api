5521fe3001ed2748cce8f1297b5b823c7b02c622
Thomas <tai.okubo93@gmail.com>
2018-10-20 23:34:02 -0300

    Add builder to Fighter class

--- src/main/java/com/repnation/ufc/domain/model/Fighter.java
+++ src/main/java/com/repnation/ufc/domain/model/Fighter.java
@@ -1,47 +1,131 @@
 package com.repnation.ufc.domain.model;
 
-import org.springframework.beans.factory.annotation.Value;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Fighter {
-
     @Id
     @GeneratedValue
     private Long Id;
-
-    @Value("first_name")
+    @Column(name = "first_name")
     private String firstName;
-
-    @Value("last_name")
+    @Column(name = "last_name")
     private String lastName;
-
     private String nickname;
-
-    @Value("holds_title")
-    private boolean holdsTitle;
-
-    @Value("weight_class")
+    @Column(name = "holds_title")
+    private Boolean holdsTitle;
+    @Column(name = "weight_class")
     private String weightClass;
-
-    private int winnings;
-
-    private int losses;
-
-    private int draws;
-
-    private int age;
-
+    private Integer winnings;
+    private Integer losses;
+    private Integer draws;
+    private Integer age;
     private String height;
-
     private String weight;
-
     private String summary;
 
     public Fighter() {
+    }
+
+    private Fighter(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.nickname = builder.nickname;
+        this.holdsTitle = builder.holdsTitle;
+        this.weightClass = builder.weightClass;
+        this.winnings = builder.winnings;
+        this.losses = builder.losses;
+        this.draws = builder.draws;
+        this.age = builder.age;
+        this.height = builder.height;
+        this.weight = builder.weight;
+        this.summary = builder.summary;
+    }
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String nickname;
+        private boolean holdsTitle;
+        private String weightClass;
+        private int winnings;
+        private int losses;
+        private int draws;
+        private int age;
+        private String height;
+        private String weight;
+        private String summary;
+
+        public Builder() {
+        }
+
+        public Builder withFirsName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder withNickname(String nickname) {
+            this.nickname = nickname;
+            return this;
+        }
+
+        public Builder withHoldsTitle(Boolean holdsTitle) {
+            this.holdsTitle = holdsTitle;
+            return this;
+        }
+
+        public Builder withWeightClass(String weightClass) {
+            this.weightClass = weightClass;
+            return this;
+        }
+
+        public Builder withWinnings(Integer winnings) {
+            this.winnings = winnings;
+            return this;
+        }
+
+        public Builder withLosses(Integer losses) {
+            this.losses = losses;
+            return this;
+        }
+
+        public Builder withDraws(Integer draws) {
+            this.draws = draws;
+            return this;
+        }
+
+        public Builder withAge(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder withHeight(String height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder withWeight(String weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Builder withSummary(String summary) {
+            this.summary = summary;
+            return this;
+        }
+
+        public Fighter build(){
+            return new Fighter(this);
+        }
     }
 
     public String getFirstName() {
@@ -72,7 +156,7 @@ public class Fighter {
         return holdsTitle;
     }
 
-    public void setHoldsTitle(boolean holdsTitle) {
+    public void setHoldsTitle(Boolean holdsTitle) {
         this.holdsTitle = holdsTitle;
     }
 
@@ -88,7 +172,7 @@ public class Fighter {
         return winnings;
     }
 
-    public void setWinnings(int winnings) {
+    public void setWinnings(Integer winnings) {
         this.winnings = winnings;
     }
 
@@ -96,7 +180,7 @@ public class Fighter {
         return losses;
     }
 
-    public void setLosses(int losses) {
+    public void setLosses(Integer losses) {
         this.losses = losses;
     }
 
@@ -104,7 +188,7 @@ public class Fighter {
         return draws;
     }
 
-    public void setDraws(int draws) {
+    public void setDraws(Integer draws) {
         this.draws = draws;
     }
 
@@ -112,7 +196,7 @@ public class Fighter {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
