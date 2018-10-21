22a06f9f4e4f72cbadaa075ba6140bbdd73ac3f5
Thomas <tai.okubo93@gmail.com>
2018-10-21 12:06:32 -0300

    Update FighterController to user FighterVo

--- src/test/java/com/repnation/ufc/controller/FighterControllerTest.java
+++ src/test/java/com/repnation/ufc/controller/FighterControllerTest.java
@@ -1,7 +1,9 @@
 package com.repnation.ufc.controller;
 
 import com.repnation.ufc.domain.model.Fighter;
+import com.repnation.ufc.domain.model.vo.FighterVo;
 import com.repnation.ufc.service.FighterService;
+import com.repnation.ufc.service.mapper.FighterMapper;
 import javassist.NotFoundException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,37 +30,55 @@ public class FighterControllerTest {
     @Test
     public void whenIRequestForAllFightersItShouldPerformProperly() {
         List<Fighter> fighters = new ArrayList<>();
-        Fighter conorMcGregor = new Fighter();
-        conorMcGregor.setNickname("Notorious");
-        fighters.add(conorMcGregor);
+        Fighter fighter = new Fighter.Builder()
+                .withFirsName("Conor")
+                .withLastName("McGregor")
+                .withNickname("Notorious")
+                .build();
+        fighters.add(fighter);
+
+        List<FighterVo> fightersVo = new ArrayList<>();
+        FighterVo expectedFighterVo = new FighterVo.Builder()
+                .withFirsName("Conor")
+                .withLastName("McGregor")
+                .withNickname("Notorious")
+                .build();
+        fightersVo.add(expectedFighterVo);
 
         when(fighterService.findAll()).thenReturn(fighters);
+        when(FighterMapper.mapFromDomainToVo(any())).thenReturn(expectedFighterVo);
 
-        List<Fighter> actualFighters = fighterController.getFighters();
+        List<FighterVo> actualFighters = fighterController.getFighters();
 
-        assertEquals(fighters, actualFighters);
+        assertEquals(fightersVo, actualFighters);
         assertEquals(Integer.parseInt("1"), actualFighters.size());;
     }
 
     @Test
     public void whenIRequestFighterByIdShouldPerformProperly() throws Exception{
-        Fighter expectedFighter = new Fighter();
-        expectedFighter.setNickname("Notorious");
+        Fighter expectedFighter = new Fighter.Builder()
+                .withFirsName("Anderson")
+                .withLastName("Silva")
+                .withNickname("Spider")
+                .build();
+
+        FighterVo expectedFighterVo= new FighterVo.Builder()
+                .withFirsName("Anderson")
+                .withLastName("Silva")
+                .withNickname("Spider")
+                .build();
 
         when(fighterService.findById(any())).thenReturn(expectedFighter);
+        when(FighterMapper.mapFromDomainToVo(any())).thenReturn(expectedFighterVo);
 
-        Fighter actualFighter = fighterController.getFighterById(Long.valueOf(1));
+        FighterVo actualFighter = fighterController.getFighterById(Long.valueOf(1));
 
         assertEquals(expectedFighter, actualFighter);
     }
 
     @Test(expected = NotFoundException.class)
     public void whenIRequestForAnInvalidFighterShouldThrowException() throws Exception{
-        Fighter expectedFighter = new Fighter();
-        expectedFighter.setNickname("Notorious");
-
         when(fighterService.findById(any())).thenThrow(new NotFoundException("Fighter Not Found"));
-
-        Fighter actualFighter = fighterController.getFighterById(Long.valueOf(1));
+        FighterVo actualFighter = fighterController.getFighterById(Long.valueOf(1));
     }
 }
