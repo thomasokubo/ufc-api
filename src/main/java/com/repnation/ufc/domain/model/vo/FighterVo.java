2a9427b548cf0abd335845dc6d93aab2bb1798a0
Thomas <tai.okubo93@gmail.com>
2018-10-21 12:05:43 -0300

    Add builder to FighterVo

--- src/main/java/com/repnation/ufc/domain/model/vo/FighterVo.java
+++ src/main/java/com/repnation/ufc/domain/model/vo/FighterVo.java
@@ -21,4 +21,100 @@ public class FighterVo {
     private String weight;
     private String summary;
 
+    private FighterVo(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.nickname = builder.nickname;
+        this.holdsTitle = builder.holdsTitle;
+        this.weightClass = builder.weightClass;
+        this.winnings = builder.winnings;
+        this.losses = builder.losses;
+        this.draws = builder.draws;
+        this.age = builder.age;
+        this.height = builder.height;
+        this.weight = builder.weight;
+        this.summary = builder.summary;
+    }
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String nickname;
+        private boolean holdsTitle;
+        private String weightClass;
+        private int winnings;
+        private int losses;
+        private int draws;
+        private int age;
+        private String height;
+        private String weight;
+        private String summary;
+
+        public Builder() {
+        }
+
+        public Builder withFirsName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder withNickname(String nickname) {
+            this.nickname = nickname;
+            return this;
+        }
+
+        public Builder withHoldsTitle(Boolean holdsTitle) {
+            this.holdsTitle = holdsTitle;
+            return this;
+        }
+
+        public Builder withWeightClass(String weightClass) {
+            this.weightClass = weightClass;
+            return this;
+        }
+
+        public Builder withWinnings(Integer winnings) {
+            this.winnings = winnings;
+            return this;
+        }
+
+        public Builder withLosses(Integer losses) {
+            this.losses = losses;
+            return this;
+        }
+
+        public Builder withDraws(Integer draws) {
+            this.draws = draws;
+            return this;
+        }
+
+        public Builder withAge(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder withHeight(String height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder withWeight(String weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Builder withSummary(String summary) {
+            this.summary = summary;
+            return this;
+        }
+
+        public FighterVo build() {
+            return new FighterVo(this);
+        }
+    }
 }
