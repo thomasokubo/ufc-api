5134386a0386083880303226c50ae63c19b4c0ce
Thomas <tai.okubo93@gmail.com>
2018-11-05 20:45:03 -0200

    Fix FighterController dependency

--- src/main/java/com/repnation/ufc/controller/FighterController.java
+++ src/main/java/com/repnation/ufc/controller/FighterController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
