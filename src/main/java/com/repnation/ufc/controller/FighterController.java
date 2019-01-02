e56e0cd44508703e068ed884ff401317744dcf3f
Thomas Okubo <tai.okubo93@gmail.com>
2019-01-02 16:04:33 -0200

    add new comment

--- src/main/java/com/repnation/ufc/controller/FighterController.java
+++ src/main/java/com/repnation/ufc/controller/FighterController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
-// Comments
+// New Comments
 @Api(value = "ufc-api", description = "Operations related to the fighters")
 @RestController
 @RequestMapping("/fighters")
